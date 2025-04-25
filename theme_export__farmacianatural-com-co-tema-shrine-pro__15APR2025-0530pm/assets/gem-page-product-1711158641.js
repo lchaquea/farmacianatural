ce8e72ec40cb0b24491aa77d45ac4313e9123617
Lucas Chaquea <lmchaquea@gmail.com>
2025-04-25 11:03:48 -0500

    Add V10 product template with all required sections

---
+++ theme_export__farmacianatural-com-co-tema-shrine-pro__15APR2025-0530pm/assets/gem-page-product-1711158641.js
@@ -0,0 +1,18 @@
+
+
+    
+  
+    
+  
+    
+  window.__gfAnimate.registerPageLoaded(jQuery("#e-1711159263563"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"pulse","delay":0,"duration":500,"iteration-count":"infinite"},"interaction":{"key":"after-page-loaded","type":"level2"}}');
+    
+  
+    
+  window.__gfAnimate.registerPageLoaded(jQuery("#e-1711159291283"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"pulse","delay":0,"duration":500,"iteration-count":"1"},"interaction":{"key":"after-page-loaded","type":"level2"}}');
+    
+  
+    
+  
+    
+  
